9fd6ad228461e259e74974c6aa9b05f06a45bbce
InTheFoldTCG <info@inthefoldtcg.com>
2026-08-24 17:07:07 -0700

    Update customcard.tsx

--- src/pages/customcard.tsx
+++ src/pages/customcard.tsx
@@ -47,7 +47,7 @@ export default function CustomCard() {
   setIsSubmitting(true);
   try {
     const photoUrl = photo ? URL.createObjectURL(photo) : "No photo uploaded";
-    const endpoint = "https://script.google.com/macros/s/AKfycbyb-Us51UIh1Jgmx8w9IopXePiZrWqVzIzYMgBsUXgMi964zGpZwUUYlxf7C6TRIFym/exec";
+   const endpoint = "https://script.google.com/macros/s/AKfycbwpRx8dRESn7sZSfb-o4QTaem52SBaajsSVkHnbd3WFuc0dxgpF1j1OBrGh7Mm-BgznEQ/exec";
     
     // Generate a unique order ID
     const orderId = `ITF-${Math.floor(100000 + Math.random() * 900000)}`;
